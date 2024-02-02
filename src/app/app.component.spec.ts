44f3e35b892ce0b5c605492aa42c1248caaf6401
Nicholas Thompson <ntho2265@students.vsu.edu>
2024-02-01 20:46:08 -0500

    Updated 'should render title' test
    Updated 'should render title' test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Project-1');
   });
 
-  it('should render title', () => {
+  it('should render tab', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Project-1');
+    expect(compiled.querySelector('div.tab > button')?.textContent).toContain('Current Data');
   });
 });
