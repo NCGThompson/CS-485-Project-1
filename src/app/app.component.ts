de96ad80234550844f6ff7cff96d619c3f84b750
LDePriest <87504343+LDePriest@users.noreply.github.com>
2024-02-09 19:40:41 -0500

    review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+//importing from libaries
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common'
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,AppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Project-1';
+  title = 'Project-1'; 
 }
