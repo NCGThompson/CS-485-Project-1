ccd44daacdbb8fc32ebfe556962a0a9f13990f74
Nicholas Thompson <ntho2265@students.vsu.edu>
2024-03-01 15:32:51 -0500

    unit tests for xml-dom-display

--- src/app/xml-dom-display/xml-dom-display.component.spec.ts
+++ src/app/xml-dom-display/xml-dom-display.component.spec.ts
@@ -4,7 +4,6 @@ import { SimpleChange } from '@angular/core';
 import { XmlDomDisplayComponent } from './xml-dom-display.component';
 
 import xmlText1 from 'raw-loader!../../assets/test-data/current.xml';
-import xmlText2 from 'raw-loader!../../assets/test-data/sample.xml';
 
 describe('XmlDomDisplayComponent', () => {
   let component: XmlDomDisplayComponent;
@@ -24,7 +23,7 @@ describe('XmlDomDisplayComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render current data', () => {
+  it('should render component names', () => {
     const parser = new DOMParser();
     const dom = parser.parseFromString(xmlText1, 'text/xml');
     component.xmlDocument = dom;
@@ -34,34 +33,7 @@ describe('XmlDomDisplayComponent', () => {
     });
     fixture.detectChanges();
     const componentElement = fixture.nativeElement as HTMLElement;
-    for (const a of [
-      'xml-dom-display works!',
-      'PathFeedrateOverride',
-      'Timestamp',
-      '2024-02-13T04:00:56.979169',
-      '248496697',
-    ]) {
-      expect(componentElement.textContent).toContain(a);
-    }
-  });
-
-  it('should render sample data', () => {
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(xmlText2, 'text/xml');
-    component.xmlDocument = dom;
-
-    component.ngOnChanges({
-      ['xmlDocument']: new SimpleChange(undefined, dom, false),
-    });
-    fixture.detectChanges();
-    const componentElement = fixture.nativeElement as HTMLElement;
-    for (const a of [
-      'Linear : Z',
-      'Timestamp',
-      '2024-02-16T11:33:09.624245',
-      '251242059',
-      'Samples',
-    ]) {
+    for (const a of ['controller_basic', 'hydraulic', 'Hurco01']) {
       expect(componentElement.textContent).toContain(a);
     }
   });
